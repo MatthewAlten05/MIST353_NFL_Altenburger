b38fa9af91d965e05c3cfff96b4d6eefbd44b8af
MatthewAlten05 <rooneyaltenburger@gmail.com>
2026-02-17 12:47:14 -0500

    edited table data

--- Data/CreateTablesAltenburger.sql
+++ Data/CreateTablesAltenburger.sql
@@ -1,5 +1,11 @@
 use NFL_RDB_Altenburger;
 
+if(object_id('Team', 'U') is not null)
+    drop table Team;
+if(object_id('ConferenceDivision', 'U') is not null)
+    drop table ConferenceDivision;
+
+
 GO
 create table ConferenceDivision (
     ConferenceID int IDENTITY(1,1)
@@ -8,8 +14,13 @@ create table ConferenceDivision (
         CONSTRAINT CK_ConferenceNames CHECK (Conference IN ('AFC', 'NFC')),
     Division NVARCHAR(50) not null
         CONSTRAINT CK_DivisionNames CHECK (Division IN ('North', 'South', 'East', 'West'))
+        constraint UQ_ConferenceDivision unique (Conference, Division);
 );
-GO
+
+alter table ConferenceDivision
+    add constraint CK_ConferenceDivision CHECK (
+       
+
 create table Team (
     TeamID int IDENTITY(1,1)
         CONSTRAINT PK_Team primary key,
--- Data/InsertDataAltenburger.sql
+++ Data/InsertDataAltenburger.sql
@@ -1,6 +1,40 @@
-INSERT into Team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) values
-('Baltimore Ravens', 'Baltimore, MD', 'Purple, Black', 2),
-('Cincinnati Bengals', 'Cincinnati, OH', 'Orange, White', 2),
-('Cleveland Browns', 'Cleveland, OH', 'Brown, White', 2),
-('Pittsburgh Steelers', 'Pittsburgh, PA', 'Black, Gold', 2)
-;
+insert into team (TeamName, TeamCityState, TeamColors, ConferenceDivisionID) values
+('Baltimore Ravens', 'Baltimore, MD', 'Purple', 1),
+('Cincinnati Bengals', 'Cincinnati, OH', 'Orange', 1),
+('Cleveland Browns', 'Cleveland, OH', 'Brown,White', 1),
+('Pittsburgh Steelers', 'Pittsburgh, PA', 'Black, Gold', 1),
+
+('Houston Texans', 'Houston, TX', 'Deep Steel Blue, Battle Red, Liberty White', 2),
+('Indianapolis Colts', 'Indianapolis, IN', 'Royal Blue, White', 2),
+('Jacksonville Jaguars', 'Jacksonville, FL', 'Teal, Black, Gold', 2),
+('Tennessee Titans', 'Nashville, TN', 'Navy, Light Blue, Red, Silver', 2),
+
+('Buffalo Bills', 'Buffalo, NY', 'Royal Blue, Red, White', 3),
+('Miami Dolphins', 'Miami, FL', 'Aqua, Orange, White', 3),
+('New England Patriots', 'Foxborough, MA', 'Navy Blue, Red, Silver', 3),
+('New York Jets', 'East Rutherford, NJ', 'Gotham Green, Spotlight White, Stealth Black', 3),
+
+('Denver Broncos', 'Denver, CO', 'Orange, Navy Blue, White', 4),
+('Kansas City Chiefs', 'Kansas City, MO', 'Red, Gold, White', 4),
+('Las Vegas Raiders', 'Las Vegas, NV', 'Silver, Black', 4),
+('Los Angeles Chargers', 'Los Angeles, CA', 'Powder Blue, Sunshine Gold, White', 4),
+
+('Dallas Cowboys', 'Arlington, TX', 'Navy Blue, Metallic Silver, White', 5),
+('New York Giants', 'East Rutherford, NJ', 'Royal Blue, Red, White', 5),
+('Philadelphia Eagles', 'Philadelphia, PA', 'Midnight Green, Silver, Black, White', 5),
+('Washington Commanders', 'Landover, MD', 'Burgundy and Gold', 5),
+
+('Chicago Bears', 'Chicago, IL', 'Navy Blue, Orange, White', 6),
+('Detroit Lions', 'Detroit, MI', 'Honolulu Blue, Silver, White', 6),
+('Green Bay Packers', 'Green Bay, WI', 'Dark Green, Gold, White', 6),
+('Minnesota Vikings', 'Minneapolis, MN', 'Purple, Gold, White', 6),
+
+('Atlanta Falcons', 'Atlanta, GA', 'Red, Black, White', 7),
+('Carolina Panthers', 'Charlotte, NC', 'Black, Panther Blue, Silver, White', 7),
+('New Orleans Saints', 'New Orleans, LA', 'Old Gold and Black', 7),
+('Tampa Bay Buccaneers', 'Tampa Bay Area, FL', 'Red, Pewter and Black', 7),
+
+('Arizona Cardinals', 'Phoenix, AZ', 'Cardinal Red and White', 8),
+('Los Angeles Rams', 'Los Angeles, CA', 'Royal Blue and Solor Gold', 8),
+('San Francisco 49ers', 'San Francisco, CA', 'Scarlet Red and Gold', 8),
+('Seattle Seahawks', 'Seattle, WA', 'College Navy, Action Green, Wolf Grey', 8);
