c9541f5a3e2ee34a3204d9945c4239d7c27fee66
MatthewAlten05 <rooneyaltenburger@gmail.com>
2026-04-20 22:51:57 -0400

    fix

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -15,7 +15,7 @@ def get_db_connection():
         raise Exception("Missing database environment variables")
 
     connection_string = (
-        "DRIVER={ODBC Driver 18 for SQL Server};"
+        "DRIVER={ODBC Driver 17 for SQL Server};"
         f"SERVER={server},1433;"
         f"DATABASE={database};"
         f"UID={username};"
