9c8974f8e9f59cf27e4d0f27ca334ad315b4b0c8
MatthewAlten05 <rooneyaltenburger@gmail.com>
2026-04-23 12:10:34 -0400

    connection change

--- API/get_teams_by_color.py
+++ API/get_teams_by_color.py
@@ -1,21 +1,18 @@
 from get_db_connection import get_db_connection
 
-def get_teams_by_color(
-      color: str
-):
-
+def get_teams_by_color(color: str):
     conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("{call procGetTeamsByColor(?)}", (color,))
+    cursor = conn.cursor(as_dict=True)
+
+    cursor.execute("EXEC procGetTeamsByColor %s", (color,))
     rows = cursor.fetchall()
     conn.close()
 
-    results  = [
+    results = [
         {
-            "TeamName": row.Team_Name,
-            "TeamColors": row.Colors
+            "TeamName": row["TeamName"],
+            "TeamColors": row["TeamColors"]
         }
-
         for row in rows
     ]
 
