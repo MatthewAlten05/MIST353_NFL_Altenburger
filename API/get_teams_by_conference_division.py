fa117e14f5f7501f342fa8180226190ddba9c541
MatthewAlten05 <rooneyaltenburger@gmail.com>
2026-04-28 10:44:51 -0400

    fixes

--- API/get_teams_by_conference_division.py
+++ API/get_teams_by_conference_division.py
@@ -2,30 +2,21 @@ from get_db_connection import get_db_connection
 
 def get_teams_by_conference_division(conference=None, division=None):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(as_dict=True)
 
     cursor.execute(
-        "EXEC dbo.procGetTeamsByConferenceDivision @Conference=?, @Division=?",
+        "EXEC dbo.procGetTeamsByConferenceDivision @Conference=%s, @Division=%s",
         (conference, division)
     )
 
     rows = cursor.fetchall()
 
-    results = []
-    for row in rows:
-        results.append({
-            "TeamName": row[0],
-            "TeamColors": row[1],
-            "Conference": row[2],
-            "Division": row[3]
-        })
-
+    cursor.close()
     conn.close()
-    return results
 
-#this is a test call to the function, you can replace the parameters with actual values
+    return {"data": rows}
+
 
 if __name__ == "__main__":
     teams = get_teams_by_conference_division("NFC", "North")
-    for team in teams:
-        print(team)
+    print(teams)
