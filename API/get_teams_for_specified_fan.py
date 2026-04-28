3a5959ec592564f7c591b95e1ec6342ffb7d8b5d
MatthewAlten05 <rooneyaltenburger@gmail.com>
2026-04-28 11:42:38 -0400

    changes

--- API/get_teams_for_specified_fan.py
+++ API/get_teams_for_specified_fan.py
@@ -1,22 +1,17 @@
-
 from get_db_connection import get_db_connection
-def get_teams_for_specified_fan(
-    NFLFanID: int
-):
+
+def get_teams_for_specified_fan(NFLFanID: int):
     conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("{call procGetTeamsForSpecifiedFan(?)}", (NFLFanID,))
+    cursor = conn.cursor(as_dict=True)
+
+    cursor.execute(
+        "EXEC dbo.procGetTeamsForSpecifiedFan %s",
+        (NFLFanID,)
+    )
+
     rows = cursor.fetchall()
+
+    cursor.close()
     conn.close()
 
-    results = [
-        {
-            "TeamName": row.Tname,
-            "Conference": row.Conference,
-            "Division": row.Division,
-            "TeamColors": row.Tcolors
-        }
-        for row in rows
-    ]
-
-    return {"data": results}
+    return {"data": rows}
--- API/get_teams_in_same_conference_division_as_specified_team.py
+++ API/get_teams_in_same_conference_division_as_specified_team.py
@@ -2,29 +2,21 @@ from get_db_connection import get_db_connection
 
 def get_teams_in_same_conference_division_as_specified_team(team_name):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(as_dict=True)
 
     cursor.execute(
-        "EXEC dbo.procGetTeamsInSameConferenceDivisionAsSpecifiedTeam @TeamName=?",
+        "EXEC dbo.procGetTeamsInSameConferenceDivisionAsSpecifiedTeam @TeamName=%s",
         (team_name,)
     )
 
     rows = cursor.fetchall()
 
-    results = []
-    for row in rows:
-        print(row)  # DEBUG - remove later
-
-        results.append({
-            "TeamName": row[0],
-            "Conference": row[1],
-            "Division": row[2]
-        })
-
+    cursor.close()
     conn.close()
-    return results
+
+    return {"data": rows}
+
 
 if __name__ == "__main__":
     teams = get_teams_in_same_conference_division_as_specified_team("Pittsburgh Steelers")
-    for team in teams:
-        print(team)
+    print(teams)
