a610dd3db2eda3733aad7b72e63850fa0372e313
MatthewAlten05 <rooneyaltenburger@gmail.com>
2026-05-01 16:16:47 -0400

    work?

--- API/get_all_teams.py
+++ API/get_all_teams.py
@@ -1,14 +1,14 @@
 from get_db_connection import get_db_connection
-import pymssql
 
 def get_all_teams():
     conn = get_db_connection()
     cursor = conn.cursor(as_dict=True)
-    cursor.callproc("procGetAllTeams")
+
+    cursor.execute("EXEC dbo.procGetAllTeams")
     rows = cursor.fetchall()
+
     conn.close()
 
-    #Convert pymssql.Row objects to dicts
     results = [
         {
             "TeamID": row["TeamID"],
